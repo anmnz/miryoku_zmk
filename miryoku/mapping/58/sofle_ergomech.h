e7c9c52e7c252e82a0b295a04aa71e43dcf6a554
anmnz <anmnz@pobox.com>
2026-01-02 18:51:33 +0000

    Disable Ergomech Sofle 5-way switch

--- miryoku/mapping/58/sofle_ergomech.h
+++ miryoku/mapping/58/sofle_ergomech.h
@@ -14,7 +14,8 @@ XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29  XXX \
 MUTE      XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX \
-                                        &kp ENTER  &kp RIGHT  &kp UP_ARROW  &kp LEFT  &kp DOWN
+                                        XXX XXX XXX XXX XXX
+//                                      &kp ENTER  &kp RIGHT  &kp UP_ARROW  &kp LEFT  &kp DOWN
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 52 53
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 54 55
