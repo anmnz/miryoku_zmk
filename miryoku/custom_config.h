561a02d9a58d701e841904e8d2d757454bef4e95
anmnz <anmnz@pobox.com>
2026-01-02 20:59:10 +0000

    Ergomech Sofle roller encoder binding

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,1 +1,13 @@
 #define MIRYOKU_ALPHAS_QWERTY
+
+#include <behaviors.dtsi>
+
+// up/down for roller encoder
+/ {
+    keymap {
+        BASE {
+            sensor-bindings = <&inc_dec_kp C_VOL_DN C_VOL_UP>;
+        };
+    };
+};
+
